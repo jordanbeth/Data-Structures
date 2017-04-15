93a4899f0ea6066820085ecf20eb3375249ee6b2
Jordan Beth <jordanbeth14@gmail.com>
2017-04-15 15:33:29 -0400

    finished stack and queue

--- Queue.js
+++ Queue.js
@@ -3,47 +3,49 @@
 // Enqueue - add item to end of Queue
 // Dequeue - remove item from beginnign of Queue
 
-function Queue(capacity) {
-    // private properties
-    this._capacity = capacity || Infinity;
-    this._waitingLine = {};
-    this._head = 0;
-    this._tail = 0;
-};
+class Queue {
+    constructor(capacity) {
+        this._capacity = capacity || Infinity;
+        this._waitingLine = {};
+        this._head = 0;
+        this._tail = 0;
+    };
 
-Queue.prototype.enqueue = function(value) {
-    if (this.count() < this._capacity) {
-        this._waitingLine[this._tail++] = value;
-        return this.count();
+    enqueue(value) {
+        if (this.count() < this._capacity) {
+            this._waitingLine[this._tail++] = value;
+            return this.count();
+        }
+        return "Error: Queue is full!!!"
+    };
+
+    dequeue() {
+        let element = this._waitingLine[this._head];
+        delete this._waitingLine[this._head];
+        if (this._head < this._tail) this._head++;
+        return element;
+    };
+
+    peek() {
+        return this._waitingLine[this._head];
+    };
+    
+    count() {
+        return this._tail - this._head;
+    };
+
+    contains(value) {
+        for (let i = this._head; i < this._tail; i++) {
+            if (this._waitingLine[i] === value) return true;
+        }
+        return false
     }
-    return "Error: Queue is full!!!"
-};
 
-Queue.prototype.dequeue = function() {
-    let element = this._waitingLine[this._head];
-    delete this._waitingLine[this._head];
-    if (this._head < this._tail) this._head++;
-    return element;
-};
+    until(value) {
+        for (let i = this._head; i < this._tail; i++) {
+            if (this._waitingLine[i] === value) return i - this._head + 1;
+        }
+        return null;
+    };
 
-Queue.prototype.peek = function() {
-    return this._waitingLine[this._head];
-};
-
-Queue.prototype.count = function() {
-    return this._tail - this._head;
-};
-
-Queue.prototype.contains = function(value) {
-    for (let i = this._head; i < this._tail; i++) {
-        if (this._waitingLine[i] === value) return true;
-    }
-    return false;
-};
-
-Queue.prototype.until = function(value) {
-    for (let i = this._head; i < this._tail; i++) {
-        if (this._waitingLine[i] === value) return i - this._head + 1;
-    }
-    return null;
 };
--- Stack.js
+++ Stack.js
@@ -2,35 +2,35 @@
 // LIFO - Last In First Out
 // Push - add item to the end of the Stack
 // Pop - remove the last item from the Stack
-function Stack(capacity) {
-    this._capacity = capacity || Infinity;
-    this._storage = {};
-    this._count = 0;
-};
+class Stack {
+    constructor(capacity) {
+        this._capacity = capacity || Infinity;
+        this._storage = {};
+        this._count = 0;
+    };
 
-// O(1)
-Stack.prototype.push = function(value) {
-    if (this._count < this._capacity) {
-        this._storage[this._count++] = value;
+    push(value) {
+        if (this._count < this._capacity) {
+            this._storage[this._count++] = value;
+            return this._count;
+        }
+        return "Error: Max Capacity reached.";
+    }
+
+    pop() {
+        let value = this._storage[--this._count];
+        delete this._storage[this._count];
+        if (this._count < 0) {
+            this._count = 0
+        }
+        return value;
+    };
+
+    peek() {
+        return this._storage[this._count - 1];
+    };
+
+    count() {
         return this._count;
-    }
-    return "Error: Max Capacity reached."
-};
-
-// O(1)
-Stack.prototype.pop = function() {
-    let value = this._storage[--this._count];
-    delete this._storage[this._count];
-    if (this._count < 0) {
-        this._count = 0
-    }
-    return value;
-};
-
-Stack.prototype.peek = function() {
-    return this._storage[this._count - 1];
-};
-
-Stack.prototype.count = function() {
-    return this._count;
+    };
 };
